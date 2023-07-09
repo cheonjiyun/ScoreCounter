ecf337661b28086af0ec4655c6aa5c6427d3b992
gunbambam <gunbam715@gmail.com>
2023-07-09 16:25:02 +0900

    feat: 문제개수 SharedPreferences적용

--- app/src/main/java/com/jiyun/scorecounter/MainActivity.kt
+++ app/src/main/java/com/jiyun/scorecounter/MainActivity.kt
@@ -1,36 +1,39 @@
 package com.jiyun.scorecounter
 
-import android.app.Activity
+import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var reset_button = findViewById<Button>(R.id.reset_button)
+        val reset_button = findViewById<Button>(R.id.reset_button)
 
-        var question_minus_button = findViewById<Button>(R.id.question_minus_button)
-        var question_textview = findViewById<TextView>(R.id.question_textview)
-        var question_plus_button = findViewById<Button>(R.id.question_plus_button)
+        val question_minus_button = findViewById<Button>(R.id.question_minus_button)
+        val question_textview = findViewById<TextView>(R.id.question_textview)
+        val question_plus_button = findViewById<Button>(R.id.question_plus_button)
 
-        var score_textview = findViewById<TextView>(R.id.score_textview)
+        val score_textview = findViewById<TextView>(R.id.score_textview)
 
-        var plus_circle_button = findViewById<Button>(R.id.plus_circle_button) // + 버튼
-        var minus_button = findViewById<Button>(R.id.minus_button) // - 버튼
+        val plus_circle_button = findViewById<Button>(R.id.plus_circle_button) // + 버튼
+        val minus_button = findViewById<Button>(R.id.minus_button) // - 버튼
 
 
         // 초기 값 세팅
-        var question_number = 60 // 문제 개수
-        question_textview.setText(question_number.toString())
+        val questionShared: SharedPreferences = applicationContext.getSharedPreferences(
+            "questionNum",
+            Context.MODE_PRIVATE
+        ) //SharedPreferences
+        var questionNumber = questionShared.getInt("question_number", 60)   // 문제 개수
+        question_textview.setText(questionNumber.toString()) //문제 개수
 
         var count = 0 //카운트 값
         plus_circle_button.setText(count.toString())
@@ -48,33 +51,45 @@ class MainActivity : AppCompatActivity() {
             changeCount(plus_circle_button, count)
 
             //점수 재 계산
-            score = calulateScore(count, question_number, score_textview)
+            score = calulateScore(count, questionNumber, score_textview)
         }
 
         // 문제 개수 - 버튼
         question_minus_button.setOnClickListener {
-            if(question_number > count && question_number > 1){
-                question_number--
+            if (questionNumber > count && questionNumber > 1) {
+                questionNumber--
 
                 //문제 개수 표시 값 바꾸기
-                changeQuestionNumber(question_textview, question_number)
+                changeQuestionNumber(question_textview, questionNumber)
 
                 //점수 다시 계산
-                score = calulateScore(count, question_number, score_textview)
+                score = calulateScore(count, questionNumber, score_textview)
+
+                //문제 개수 SharedPreferences에 저장
+                with(questionShared.edit()) {
+                    putInt("question_number", questionNumber)
+                    apply()
+                }
             }
 
         }
 
         // 문제 개수 + 버튼
         question_plus_button.setOnClickListener {
-            if(question_number < 9999){
-                question_number++
+            if (questionNumber < 9999) {
+                questionNumber++
 
                 //문제 개수 표시 값 바꾸기
-                changeQuestionNumber(question_textview, question_number)
+                changeQuestionNumber(question_textview, questionNumber)
 
                 //점수 다시 계산
-                score = calulateScore(count, question_number, score_textview)
+                score = calulateScore(count, questionNumber, score_textview)
+
+                //문제 개수 SharedPreferences에 저장
+                with(questionShared.edit()) {
+                    putInt("question_number", questionNumber)
+                    apply()
+                }
             }
 
         }
@@ -82,7 +97,7 @@ class MainActivity : AppCompatActivity() {
 
         // 원형 버튼 클릭시
         plus_circle_button.setOnClickListener {
-            if(count < question_number){
+            if (count < questionNumber) {
                 //카운트 값 +1
                 count++
 
@@ -90,7 +105,7 @@ class MainActivity : AppCompatActivity() {
                 changeCount(plus_circle_button, count)
 
                 //점수 계산
-                score = calulateScore(count, question_number, score_textview)
+                score = calulateScore(count, questionNumber, score_textview)
             }
 
 
@@ -98,7 +113,7 @@ class MainActivity : AppCompatActivity() {
 
         // - 버튼 클릭시
         minus_button.setOnClickListener {
-            if(count > 0){
+            if (count > 0) {
                 //카운트 값 -1
                 count--
 
@@ -106,7 +121,7 @@ class MainActivity : AppCompatActivity() {
                 changeCount(plus_circle_button, count)
 
                 //점수 계산
-                score = calulateScore(count, question_number, score_textview)
+                score = calulateScore(count, questionNumber, score_textview)
             }
 
         }
@@ -114,22 +129,18 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun changeQuestionNumber(question_textview: TextView, question_number: Int){
+    fun changeQuestionNumber(question_textview: TextView, question_number: Int) {
         question_textview.setText(question_number.toString())
     }
 
-    fun changeCount(circle_button: Button, count: Int){
+    fun changeCount(circle_button: Button, count: Int) {
         circle_button.setText(count.toString())
     }
 
-    fun calulateScore(count:Int, question_number:Int, score_textview: TextView): Int {
-        var score = (count.toDouble()/question_number.toDouble() * 100).roundToInt()
+    fun calulateScore(count: Int, question_number: Int, score_textview: TextView): Int {
+        val score = (count.toDouble() / question_number.toDouble() * 100).roundToInt()
         score_textview.setText(score.toString())
         return score
     }
-
-
-    override fun onStop() {
-        super.onStop()
-    }
 }
+
